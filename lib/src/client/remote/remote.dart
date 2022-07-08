4bc9334787298df7d45946f67c5d9f334decbc94
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-07-08 18:55:56 +0200

    more WIP

--- lib/src/client/remote/remote.dart
+++ lib/src/client/remote/remote.dart
@@ -4,16 +4,34 @@ class Remote {
   final ManagedProcess _managedProcess;
 
   final String hostName;
+  final bool debugMode;
 
-  Remote(this._managedProcess, this.hostName);
+  Remote(
+    this._managedProcess,
+    this.hostName, {
+    this.debugMode = false,
+  });
 
-  Stream<String> listSnapshots(String rootDataset) =>
-      _managedProcess.run('ssh', [
-        '--',
-        hostName,
-        'zfs_backup_server',
+  Stream<String> listSnapshots(String rootDataset) => _runRemote([
         'list-snapshots',
         '--root',
         rootDataset,
       ]);
+
+  Stream<String> _runRemote(List<String> arguments) {
+    if (debugMode) {
+      return _managedProcess.run('dart', [
+        'run',
+        'bin/zfs_backup_server',
+        ...arguments,
+      ]);
+    } else {
+      return _managedProcess.run('ssh', [
+        'zfs_backup_server',
+        '--',
+        hostName,
+        ...arguments,
+      ]);
+    }
+  }
 }
