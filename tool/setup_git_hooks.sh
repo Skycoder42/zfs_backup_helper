ca62377ed811c87fb615160cb1f057b52c727ae4
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-07-08 17:31:19 +0200

    fix analysis options

--- tool/setup_git_hooks.sh
+++ tool/setup_git_hooks.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -eo pipefail
 
 mkdir -p .git/hooks
 echo "#!/bin/sh" > .git/hooks/pre-commit
 echo "set -eo pipefail" >> .git/hooks/pre-commit
 echo "exec dart run dart_pre_commit" >> .git/hooks/pre-commit
+chmod a+x .git/hooks/pre-commit
